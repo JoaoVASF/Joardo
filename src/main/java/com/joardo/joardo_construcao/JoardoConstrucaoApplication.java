9bd35c3ae92b441d0e8e9928e20e9bdde10f0ec0
João Vitor Alves <142955279+JoaoVASF@users.noreply.github.com>
2025-11-30 14:12:34 -0300

    commit inicial

---
+++ src/main/java/com/joardo/joardo_construcao/JoardoConstrucaoApplication.java
@@ -0,0 +1,13 @@
+package com.joardo.joardo_construcao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoardoConstrucaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoardoConstrucaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joardo/joardo_construcao/JoardoConstrucaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joardo.joardo_construcao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoardoConstrucaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
